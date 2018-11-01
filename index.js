41902cfc89af83964ee0ec8afe2ba7ed8cf6799f
Sam West-Sooby <s.west.sooby@gmail.com>
2018-11-01 13:51:25 +1100

    [patch] reconfigured config files and created a react specific config

--- index.js
+++ index.js
@@ -1,43 +1,41 @@
 module.exports = {
-  "extends": ["airbnb", "plugin:jest/recommended", "plugin:lodash/canonical", "plugin:prettier/recommended"],
+  "extends": ["airbnb-base", "plugin:jest/recommended", "plugin:lodash/canonical", "plugin:prettier/recommended"],
   "parser": "babel-eslint",
-  "plugins": ["jest", "jsdoc", "lodash", "json", "cypress"],
+  "plugins": ["jest", "jsdoc", "json", "lodash"],
   "env": {
     "browser": true,
-    "cypress/globals": true,
-    "jest": true,
-    "mocha": true,
-    "node": true
+    "jest": true
   },
-  "globals": {
-    "should": false,
-    "__SERVER__": false,
-    "__CLIENT__": false,
-    "__DEVELOPMENT__": false,
-    "__TEST__": false
-  },
+  "globals": {},
   "settings": {
     "lodash": {
       "version": 4
+    },
+    "import/resolver": {
+      "webpack": {
+        "config": "config/webpack.config.dev.js"
+      }
     }
   },
   "rules": {
     "func-names": "error",
-    "global-require": "error",
-    "id-length": ["error", { "min": 2, "properties": "never", "exceptions": ["_", "i", "j"] }],
-    "indent": ["error", 2, { "SwitchCase": 1 }],
-    "new-parens": "error",
+    "id-length": ["error", { "min": 2, "properties": "never", "exceptions": ["_", "P"] }],
+    "max-len": ["error", 120],
     "no-alert": "error",
     "no-console": "error",
-    "no-underscore-dangle": ["error", { "allow": ["__config", "__setMockValues"] }],
+    "no-mixed-operators": ["error", { "allowSamePrecedence": true }],
     "import/export": "error",
     "import/no-extraneous-dependencies": ["error", { "devDependencies": true }],
+    "import/order": [
+      "error",
+      {
+        "groups": [["builtin", "external"], "internal", ["parent", "sibling", "index"]],
+        "newlines-between": "always"
+      }
+    ],
     "lodash/chaining": ["error", "always"],
     "lodash/prefer-constant": ["error", false],
-    "lodash/prefer-lodash-method": ["error", { "ignoreMethods": ["find", "startsWith"] }],
-    "lodash/prefer-over-quantifier": "off",
-    "react/jsx-no-bind": "error",
-    "react/prop-types": "error",
+    "lodash/prefer-lodash-method": ["error", { "ignoreMethods": ["find"] }],
     "jsdoc/check-param-names": "error",
     "jsdoc/check-tag-names": "error",
     "jsdoc/check-types": "error",
@@ -49,26 +47,6 @@ module.exports = {
     "jsdoc/require-param": "error",
     "jsdoc/require-returns-description": "error",
     "jsdoc/require-returns-type": "error",
-
-    "react/no-unused-prop-types": "warn",
-    "jsx-a11y/href-no-hash": "warn",
-    "react/no-find-dom-node": "warn",
-    "import/prefer-default-export": "warn",
-    "react/prefer-stateless-function": "warn",
-    "react/jsx-filename-extension": "warn",
-    "react/no-string-refs": "warn",
-    "react/jsx-no-target-blank": "warn",
-    "react/require-default-props": "warn",
-    "import/first": "warn",
-    "react/forbid-prop-types": "warn",
-    "jsx-a11y/no-static-element-interactions": "warn",
-    "react/no-danger": "warn",
-    "import/order": [
-      "error",
-      {
-        "groups": [["builtin", "external"], ["parent", "sibling", "index"]],
-        "newlines-between": "always"
-      }
-    ]
+    "sort-keys": ["warn", "asc", { "caseSensitive": true, "natural": false }]
   }
 }
