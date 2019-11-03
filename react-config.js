1f3466c4eeb5bcfd3265f9bf716753421dd2972e
Cameron Chamberlain <git@camjc.com>
2019-11-04 10:31:33 +1100

    [patch] overrides to airbnb to align with our views (#187)

--- react-config.js
+++ react-config.js
@@ -18,14 +18,16 @@ module.exports = {
   plugins: ['sort-imports-es6-autofix', 'cypress', 'react-hooks', ...globalPlugins],
   rules: {
     ...globalRules,
-    'react-hooks/exhaustive-deps': 'warn',
-    'react-hooks/rules-of-hooks': 'error',
     'jsx-a11y/anchor-is-valid': 'warn',
     'jsx-a11y/no-static-element-interactions': 'warn',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react-hooks/rules-of-hooks': 'error',
     'react/forbid-prop-types': 'warn',
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'react/jsx-fragments': 'off',
     'react/jsx-no-bind': 'error',
     'react/jsx-no-target-blank': 'warn',
+    'react/jsx-props-no-spreading': 'off',
     'react/no-danger': 'warn',
     'react/no-find-dom-node': 'warn',
     'react/no-string-refs': 'warn',
@@ -33,6 +35,7 @@ module.exports = {
     'react/prefer-stateless-function': 'warn',
     'react/prop-types': 'error',
     'react/require-default-props': 'warn',
+    'react/state-in-constructor': ['error', 'never'],
     'sort-imports-es6-autofix/sort-imports-es6': [
       'error',
       {
